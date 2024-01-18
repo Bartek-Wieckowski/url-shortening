688e443f960b83f9c2adf389b18a89ef4f4c7e9b
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-18 12:00:42 +0100

    create routes and use GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles.ts';
+import Homepage from './features/Home/Homepage.tsx';
+
 function App() {
-  return <div>test</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
