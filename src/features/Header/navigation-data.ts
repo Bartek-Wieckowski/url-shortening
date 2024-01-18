843aaa2f7daae8e4d452e1f23d44cbc90f870ae3
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-18 18:47:14 +0100

    create Header component

---
+++ src/features/Header/navigation-data.ts
@@ -0,0 +1,25 @@
+export const navLinks = [
+  {
+    linkName: 'Features',
+    link: '/features',
+  },
+  {
+    linkName: 'Pricing',
+    link: '/pricing',
+  },
+  {
+    linkName: 'Resources',
+    link: '/resources',
+  },
+];
+
+export const userOptionLinks = [
+  {
+    linkName: 'Login',
+    link: '/login',
+  },
+  {
+    linkName: 'Sign Up',
+    link: '/signuo',
+  },
+];
