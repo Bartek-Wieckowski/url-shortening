2af7d8e23adcea27e58eb98d042f72c1dd4ae8c7
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-19 14:36:47 +0100

    create Statistic component and map data form file and pass data to Card component

---
+++ src/features/Statistic/statistic-data.ts
@@ -0,0 +1,21 @@
+import firstIcon from '../../assets/images/icon-brand-recognition.svg';
+import secondIcon from '../../assets/images/icon-detailed-records.svg';
+import thirdIcon from '../../assets/images/icon-fully-customizable.svg';
+
+export const statsData = [
+  {
+    icon: firstIcon,
+    title: 'Brand Recognition',
+    desc: "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.",
+  },
+  {
+    icon: secondIcon,
+    title: 'Detailed Records',
+    desc: 'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+  },
+  {
+    icon: thirdIcon,
+    title: 'Fully Customizable',
+    desc: 'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
+  },
+];
